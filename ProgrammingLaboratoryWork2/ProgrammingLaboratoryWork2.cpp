b3fa071837c28114216d0533872dbde5b514404a
fatikkkk <98143123+fatikkkk@users.noreply.github.com>
2023-11-19 22:03:10 +0300

    search max and min value

--- ProgrammingLaboratoryWork2/ProgrammingLaboratoryWork2.cpp
+++ ProgrammingLaboratoryWork2/ProgrammingLaboratoryWork2.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 using namespace std::chrono;
 
+const int sizeArray = 100;
 int minValueArr, maxValueArr;
 steady_clock::time_point startTime;
 steady_clock::time_point endTime;
@@ -24,13 +25,11 @@ void fillArray(int arr[], int sizeArr) {
 }
 
 void bruteForceSort() {
-	const int sizeArray = 100;
 	int arrayNum[sizeArray];
 	fillArray(arrayNum, sizeArray);
 	displayArray(arrayNum, sizeArray);
 
 	startTime = steady_clock::now();
-	bool isSwapped;
 	for (int i = 0; i <= sizeArray; i++)
 		for (int j = 0; j <= sizeArray; j++)
 			if (arrayNum[j] < arrayNum[i])
@@ -44,7 +43,7 @@ void bruteForceSort() {
 	displayArray(arrayNum, sizeArray);
 	cout << "Time spent Brute Force Sorting: " 
 		<< duration_cast<nanoseconds>(endTime - startTime).count() 
-		<< " nanoseconds.\n\n";
+		<< " nanoseconds.\n";
 
 }
 
@@ -52,7 +51,7 @@ void quickSort(int arr[], int left, int right) {
 	if (left > right) 
 		return;
 
-	int supportElem = arr[left + right] / 2;
+	int supportElem = arr[(left + right) / 2];
 	int leftArr = left, rightArr = right;
 
 	while (leftArr <= rightArr)
@@ -77,11 +76,33 @@ void quickSort(int arr[], int left, int right) {
 	quickSort(arr, leftArr, right);
 }
 
+void searchMinMax(int arr[], int sizeArr, bool isSort) {
+	startTime = steady_clock::now();
+	if (isSort)
+	{
+		minValueArr = arr[0];
+		maxValueArr = arr[sizeArr - 1];
+	}
+	else {
+		minValueArr = arr[0], maxValueArr = arr[0];
+		for (int i = 0; i < sizeArr; i++)
+		{
+			if (arr[i] <= minValueArr)
+				minValueArr = arr[i];
+
+			if (arr[i] >= maxValueArr)
+				maxValueArr = arr[i];
+		}
+	}
+	endTime = steady_clock::now();
+}
 int main()
 {   
     bool isLive = true;
 	while (isLive)
 	{
+		int arrayNum[sizeArray];
+		fillArray(arrayNum, sizeArray);
 		int userSelectNum;
 		cout << "Please, Select a number to choose actions with array: \n";
 		cout << "\t 1 - Brute Force Sorting\n";
@@ -94,22 +115,65 @@ int main()
 		cout << "\t 5 - Swap value\n";
 		cout << "Number: ";
 		cin >> userSelectNum;
+		cout << "\n";
 
 		switch (userSelectNum)
 		{
 		case 1:
+			cout << "================================== Brute Force Sort =================================\n";
 			bruteForceSort();
+			cout << "=====================================================================================\n\n";
 			break;
 
 		case 2:
-			const int sizeArray = 100;
-			int arrayNum[sizeArray];
-			fillArray(arrayNum, sizeArray);
+			cout << "===================================== Quick Sort ====================================\n";
 			displayArray(arrayNum, sizeArray);
+
+			startTime = steady_clock::now();
 			quickSort(arrayNum, 0, sizeArray - 1);
+			endTime = steady_clock::now();
 
 			displayArray(arrayNum, sizeArray);
-			cout << "case 2";
+			cout << "Time spent Quick Sorting: "
+				<< duration_cast<nanoseconds>(endTime - startTime).count()
+				<< " nanoseconds.\n\n";
+			cout << "=====================================================================================\n\n";
+			break;
+
+		case 3:
+			cout << "===================================== Merge Sort ====================================\n";
+			cout << "=====================================================================================\n\n";
+			break;
+
+		case 4:
+			cout << "============================== Search Min and Max value ================================\n";
+			int chooseUser;
+			bool isSort;
+			cout << "Do you want Search Min and Max in?\n";
+			cout << "\t1 - UnSorted Array\n";
+			cout << "\t2 - Sorted Array\n";
+			cout << "Number: ";
+			cin >> chooseUser;
+			switch (chooseUser)
+			{
+			case 1:
+				isSort = false;
+				searchMinMax(arrayNum, sizeArray, isSort);
+				break;
+
+			case 2:
+				quickSort(arrayNum, 0, sizeArray - 1);
+				isSort = true;
+				searchMinMax(arrayNum, sizeArray, isSort);
+				break;
+			default:
+				break;
+			}
+			cout << "Min value in array: " << minValueArr << "\n";
+			cout << "Max value in array: " << maxValueArr << "\n";
+			cout << "Time spent search min and max value in not sorting array: " << duration_cast<nanoseconds>(endTime - startTime).count() << " nanoseconds.\n";
+			cout << "=====================================================================================\n\n";
+			break;
 		default:
 			break;
 		}
